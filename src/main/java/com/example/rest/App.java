8ed22f48c55a0e9e9e97ca5849ebd574292b4b3b
hfa224 <helenffionadams@gmail.com>
2018-05-30 15:32:29 +0100

    change app name

--- src/main/java/com/example/rest/App.java
+++ src/main/java/com/example/rest/App.java
@@ -51,7 +51,7 @@ public class App {
     private static void runGetRequest() {
  
     	// sample URL
-        String url = "http://129.157.179.180:3000/shield/33/45/pink/App";
+        String url = "http://129.157.179.180:3000/shield/33/45/pink/hfa224";
         CloseableHttpResponse response = null;
  
         try {
